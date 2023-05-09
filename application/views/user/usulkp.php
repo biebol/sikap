afd6e6a03f5c65da02ca13955e373d674f78df68
garry.afrialdy <garry.afrialdy@indosiar.com>
2023-05-10 00:15:30 +0700

    lieur aink

--- application/views/user/usulkp.php
+++ application/views/user/usulkp.php
@@ -6,7 +6,7 @@
 
     <div class="row">
         <div class="col-lg-8">
-
+            <?= $this->session->flashdata('message'); ?>
             <?= form_open_multipart('user/usulkp'); ?>
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
@@ -15,13 +15,13 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">Full name</label>
+                <label for="email" class="col-sm-2 col-form-label">Full Name</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
-                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>" readonly>
+                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
             </div>
-            <div class="form-group row">
+ <!--           <div class="form-group row">
                 <div class="col-sm-2">Picture</div>
                 <div class="col-sm-10">
                     <div class="row">
@@ -36,42 +36,42 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
             <!-- Upload File Fields -->
             <div class="form-group row">
-                <label for="kep_pangkat" class="col-sm-2 col-form-label">Kep Pangkat Terakhir (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="kep_pangkat" class="col-sm-5 col-form-label">Kep Pangkat Terakhir (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="kep_pangkat" name="kep_pangkat" accept=".pdf" max-size="2048000">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="kep_jabatan" class="col-sm-2 col-form-label">Kep Jabatan Terakhir (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="kep_jabatan" class="col-sm-5 col-form-label">Kep Jabatan Terakhir (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="kep_jabatan" name="kep_jabatan" accept=".pdf" max-size="2048000">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="ijazah_dikumti" class="col-sm-2 col-form-label">Ijazah Dikumti (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="ijazah_dikumti" class="col-sm-5 col-form-label">Ijazah Dikumti (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="ijazah_dikumti" name="ijazah_dikumti" accept=".pdf" max-size="2048000">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="ijazah_ud" class="col-sm-2 col-form-label">Ijazah Ujian Dinas (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="ijazah_ud" class="col-sm-5 col-form-label">Ijazah Ujian Dinas (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="ijazah_ud" name="ijazah_ud" accept=".pdf" max-size="2048000">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="algol" class="col-sm-2 col-form-label">Algol (khusus ke III/a) (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="algol" class="col-sm-5 col-form-label">Algol (khusus ke III/a) (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="algol" name="algol" accept=".pdf" max-size="2048000">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="skp" class="col-sm-2 col-form-label">SKP 2 Thn Terakhir (PDF, max 2MB)</label>
-                <div class="col-sm-10">
+                <label for="skp" class="col-sm-5 col-form-label">SKP 2 Thn Terakhir (PDF, max 2MB)</label>
+                <div class="col-sm-6">
                     <input type="file" class="form-control-file" id="skp" name="skp" accept=".pdf" max-size="2048000">
                 </div>
             </div>
@@ -79,7 +79,8 @@
 
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <button type="submit" class="btn btn-primary">Kembali</button>
                 </div>
             </div>
 
--- application/views/user/usulkp_form.php
+++ application/views/user/usulkp_form.php
@@ -8,7 +8,19 @@
         <div class="col-lg-6">
             <?= $this->session->flashdata('message'); ?>
             <?= form_open_multipart('user/usulkp_form'); ?>
-            
+            <div class="form-group row">
+                <label for="email" class="col-sm-4 col-form-label">Email</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-sm-4 col-form-label">Full Name</label>
+                <div class="col-sm-9">
+                <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>" readonly>
+                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                </div>
+            </div>
             <div class="form-group">
                 <label for="nip">NIP</label>
                 <input type="text" class="form-control" id="nip" name="nip" value="<?= set_value('nip'); ?>">
@@ -59,10 +71,12 @@
                 </select>
                 <?= form_error('jenis_kenaikan', '<small class="text-danger">', '</small>'); ?>
             </div>
+           
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                    <button type="reset" class="btn btn-secondary">Lanjut</button>
+                    <button type="submit" class="btn btn-primary" >Simpan</button>
+                    <button type="submit" class="btn btn-primary" formaction="<?= site_url('user/usulkp'); ?>">Selanjutnya</button>
+                    <button type="reset" class="btn btn-secondary">Reset</button>
                 </div>
             </div>
 
